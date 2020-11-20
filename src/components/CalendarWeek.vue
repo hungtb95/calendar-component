1d9fb1fbf9ed55f0f4ca335273757a32f66a0f40
quang1 <quang1@gmail.com>
2020-11-20 15:37:04 +0700

    update css full calendar week

--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -1,15 +1,13 @@
 <template>
-  <div :class="['wrap-calendar-left']" :style="styles">
+  <div :class="['wrap-fullcalendar-week']" :style="styles">
     <div class="head-title-calendar d-flex">
       <span class="month-year-title mr-auto ml-auto">
         {{ $dayjs(this.datetime).format('YYYY年MM月DD日 (ddd)') }}
       </span>
-      <div class="div-img-calendar" @click="preWeek">
-        <img src="../assets/images/prev-month.png" alt srcset />
+      <div class="div-img-calendar btn-prev" @click="preWeek">
       </div>
       <button class="btn__today" @click="today">今日</button>
-      <div class="div-img-calendar" @click="nextWeek">
-        <img src="../assets/images/next-month.png" alt srcset />
+      <div class="div-img-calendar btn-next" @click="nextWeek">
       </div>
     </div>
 
@@ -165,43 +163,82 @@ $color-sunday : #ffe1e1;
 $color-saturday : #e6f7ff;
 $color-active : #51e0db;
 
-.wrap-calendar-left {
+$color-yellow: #f4eec0;
+$color-brown: #7e6752;
+$color-black: #cccccc;
+$color-gray: #eaeaea;
+$color-sunday : #ffe1e1;
+$color-saturday : #e6f7ff;
+$color-active : #51e0db;
+$color-day : #0072d2;
+$background-head-day : #f5f7f8;
+$height-day : 100px;
+$background-now : #eef5f9;
+$background-type-1 : #000000;
+$background-type-2 : #ff0000;
+$background-type-3 : #ffa500;
+$background-type-4 : #0000ff;
+$background-type-5 : #008000;
+.wrap-fullcalendar-week {
   width: 100%;
   font-size: 0.8rem;
   .head-title-calendar , .head-calendar , .body-calendar{
     display: flex;
   }
   .head-title-calendar{
-    background: $color-yellow;
     padding: 5px;
-    justify-content: center;
-    border: 1px solid $color-brown;
     .month-year-title{
       font-size: 1rem;
-      margin-left: 25px;
+      margin-left: 10px;
       margin-right: 25px;
-      margin-top: 5px;
       font-weight: bold;
+      font-size: 1.5rem;
     }
-    .div-img-calendar{
-      padding: 3px 8px;
-      background: #f2f2f2;
-      border : 1px solid #d7d7d7;
-      cursor: pointer;
-      img{
-        width: 5px;
+    .btn__today{
+      background: $color-yellow;
+      border : 2px solid $color-brown;
+      border-radius: 5px;
+      padding-left: 15px;
+      padding-right: 15px;
+      margin-left: 10px;
+      margin-right: 10px;
+
+    }
+    .btn-prev , .btn-next{
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 16px 32px 16px 0;
+      border-color: transparent $color-brown transparent transparent;
+      position: relative;
+      margin-top: 2px;
+      &::before {
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 12px 24px 12px 0;
+        border-color: transparent $color-yellow transparent transparent;
+        content : '';
+        position: absolute;
+        top: -12px;
+        right: -29px;
       }
+    }
+    .btn-next{
+       transform: rotate(180deg);
     }
   }
   .head-calendar{
     .column-cal{
       width: calc(100% / 7);
-      background: $color-gray;
-      &:first-child{
-        background: $color-sunday !important;
-      }
+      background: $color-yellow;
+      border-right: 1px solid $color-black;
+      font-weight: bold;
+      font-size: 1rem;
+      padding-top: 2px;
+      padding-bottom: 2px;
       &:last-child{
-        background: $color-saturday !important;
+        border-right: none;
       }
     }
   }
@@ -209,8 +246,6 @@ $color-active : #51e0db;
     flex-wrap: wrap;
     .day-calendar{
       width: calc(100% / 7);
-      padding-top: 5px;
-      padding-bottom: 5px;
       &:hover{
         opacity: 0.7;
         cursor: pointer;
@@ -219,29 +254,80 @@ $color-active : #51e0db;
   }
   .wrap-head-and-body{
     border: 1px solid $color-black;
-    padding: 5px;
   }
   .day-calendar{
-    background: $color-gray;
-  }
-  .is-saturday {
-    background: $color-saturday;
-  }
-  .is-holiday,
-  .is-sunday {
-    background: $color-sunday;
-  }
-  .day-active {
-    background: $color-active;
-  }
+    .box {
+      &__day {
+        border: 1px solid $color-black;
+        border-bottom: none;
+        border-right: none;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        color : $color-day;
+        background: $background-head-day;
+      }
+      &__info {
+        border: 1px solid $color-black;
+        height: $height-day;
+        border-bottom: none;
+        border-right: none;
+        padding: 10px;
+        &__item{
+          display: flex;
+          padding: 2px 5px;
+          color : #fff;
+          border-radius: 10px;
+          font-size: 0.7rem;
+          margin-bottom: 2px;
 
-  .box {
-    &__day {
-      border: 1px solid;
+          &__time{
+            margin-right: 5px;
+          }
+          &.back1{
+            background: $background-type-1;
+          }
+          &.back2{
+            background: $background-type-2;
+          }
+          &.back3{
+            background: $background-type-3;
+          }
+          &.back4{
+            background: $background-type-4;
+          }
+          &.back5{
+            background: $background-type-5;
+          }
+
+        }
+      }
     }
-    &__info {
-      border: 1px solid;
-      height: 50px;
+    &.is-saturday {
+      background: $color-saturday;
+      .box{
+        &__day{
+          background: $color-saturday;
+        }
+      }
+    }
+    &.is-sunday , &.is-holiday  {
+      background: $color-sunday;
+      .box{
+        &__day{
+           background: $color-sunday;
+        }
+      }
+    }
+    &.is-now {
+      background: $background-now;
+      .box{
+        &__day{
+          background: $background-now;
+        }
+      }
+    }
+    .day-active {
+      background: $color-active;
     }
   }
 }
